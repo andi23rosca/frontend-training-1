94f1a50fa0022425ae8ff28245eba41057f89995
Andi <andi23rosca@gmail.com>
2021-04-21 09:47:38 +0300

    Made a playground to showcase

---
+++ playground/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="bg-gray-200">Simple Vue typescript playground with Tailwind</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({});
+</script>
